08838433fda13abcb2252ca42c7893189b4e6a11
Javier Bengolea <javierbengolea@yahoo.com.ar>
2024-06-18 19:34:40 -0300

    ---

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,43 +1,54 @@
 import React from 'react'
 import "./skills.css"
-import Backend from "../../assets/backend.png"
-import Frontend from "../../assets/frontend.png"
-import DataScience from "../../assets/datascience.png"
+import Backend from "../../assets/logo_steam.png"
+import Frontend from "../../assets/seguridad_vial.png"
+import DataScience from "../../assets/subway.jpg"
 
 const Skills = () => {
     return (
         <section id="skills">
-            <span className="skillTitle">What I do</span>
+            <span className="skillTitle">Proyectos</span>
             <span className="skillDesc">
-                Soy un apasionado de Ciencias de la Computación, desarrollador por vocación, descubridor del emocionante mundo del Data Science  y Machine Learning , en el que me siento inmerso con ganas de aplicar mis capacidades, estudios y experiencia, para resolver problemas y aprovechar las posibilidades de este campo cada vez más interesante. 💖💻👨🏻‍🔬🦾
-                <br />
-                I am a Computer Science enthusiast, developer by vocation, and a discoverer of the thrilling world of Data Science and Machine Learning,  in which I feel immersed and eager to apply my skills, studies, and experience to solve problems and take advantage of the possibilities in this increasingly interesting field.💖💻👨🏻‍🔬🦾
+                Como DataScience tengo 3 proyectos Principales
+                
             </span>
             <span className="skillBars">
                 <div className="skillBar">
-                    <img src={Backend} alt="Back End" className="skillBarImg" />
+                    <img src={Backend} alt="Mlops" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h1>Back End</h1>
+                        <h1>MLOps Steam</h1>
                         <p>
-                            Back-end development focuses on the server-side of web applications, handling data management, server logic, and application functionality. It involves using server-side languages like Python, Ruby, Java, PHP, or Node.js, and databases such as MySQL, PostgreSQL, MongoDB, or Oracle. Back-end developers ensure smooth communication between the server and front-end, managing user requests, processing transactions, and maintaining authentication and authorization to keep the application functional and secure.
+                        Basados en datasets públicos sobre Steam, se realiza un EDA y se desarrolla un Sistema de Recomendación.
+                        </p>
+                        <p>
+                            <a href="https://github.com/javierbengolea/PI_ML_OPS/" target='blank' style={{color: "white", textDecoration: 'underline', fontWeight:'bold'}}
+                            >Link del Proyecto</a>
                         </p>
                     </div>
                 </div>
                 <div className="skillBar">
-                    <img src={Frontend} alt="Front End" className="skillBarImg" />
+                    <img src={Frontend} alt="Accidentes Viales" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h1>Front End</h1>
+                        <h1>Estudio Accidentes Viales</h1>
                         <p>
-                            Front-end development deals with the client-side of web applications, focusing on the visual and interactive aspects that users interact with directly. It involves using HTML, CSS, and JavaScript to create and design web pages, ensuring they are responsive and user-friendly. Front-end developers work with frameworks and libraries such as React, Angular, or Vue.js to build dynamic and interactive interfaces. Their role is to ensure that the website looks good and functions correctly on all devices, providing a seamless and engaging user experience.
+                        Es el estudio de la Accidentología Vial de la Ciudad Autónoma de Buenos Aires con el objetivo de tener una idea clara de este fenómeno para tomar medidas con perspectiva de aligeramiento de estos siniestros.
+                        </p>
+                        <p>
+                            <a href="https://github.com/javierbengolea/proyecto-accidentes-viales" target='blank' style={{color: "white", textDecoration: 'underline', fontWeight:'bold'}}
+                            >Link del Proyecto</a>
                         </p>
                     </div>
                 </div>
                 <div className="skillBar">
                     <img src={DataScience} alt="Data Science" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h1>Data Science</h1>
+                        <h1>Análisis de Opiniones y Recomendaciones</h1>
                         <p>
-                            Data science involves extracting insights and knowledge from data using various scientific methods, processes, algorithms, and systems. It combines techniques from statistics, computer science, and domain-specific knowledge to analyze and interpret complex data sets. Data scientists use programming languages such as Python, R, and SQL, along with tools like TensorFlow, PyTorch, and Pandas, to perform tasks such as data cleaning, analysis, visualization, and building predictive models. Their work helps organizations make data-driven decisions, uncover trends, and solve complex problems by transforming raw data into actionable insights.
+                        Proyecto que recorre todo el proceso desde la adquisición de datos hasta crear un Dashboard y con la ayuda de Machine Learning, usando la automatización, con el objetivo de conocer la situación y medir el desempeño. Con los Datasets de Google, Yelp y otras fuentes, obtenemos información sobre Subway y su competencia, se desarrolla un Dashboard con 4 KPI's y con la ayuda del Machine Learning se despliega un modelo predictivo que basado en las características de cada restaurante permite elegir aquellas que puedan lograr una mejora en las ratings, mejora que va acorde a los objetivos de la empresa.
+                        </p>
+                        <p>
+                            <a href="https://github.com/Aspirina180mg/PF_DATAPT07" target='blank' style={{color: "white", textDecoration: 'underline', fontWeight:'bold'}}
+                            >Link del Proyecto</a>
                         </p>
                     </div>
                 </div>
